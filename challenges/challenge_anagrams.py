5daeea9d3acaf5013d62fe38227650c13c5091b0
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-06-11 02:12:15 -0300

    requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,26 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if first_string == "" and second_string == "":
+        return ("", "", False)
+
+    sorted_first_string = quicksort_string(first_string.lower())
+    sorted_second_string = quicksort_string(second_string.lower())
+
+    return (
+        sorted_first_string,
+        sorted_second_string,
+        sorted_first_string == sorted_second_string,
+    )
+
+
+def quicksort_string(s):
+    if len(s) <= 1:
+        return s
+    pivot = s[0]
+    left = ""
+    right = ""
+    for c in s[1:]:
+        if c < pivot:
+            left += c
+        else:
+            right += c
+    return quicksort_string(left) + pivot + quicksort_string(right)
