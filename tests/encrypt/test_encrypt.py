40a52c2d0bfaecc01a97671f6ed654dc93c510a1
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-06-11 01:32:21 -0300

    Requisitos 1, 2 e 3 feitos

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(0, "string")
+
+   
+    assert encrypt_message("teste", -1) == "etset"   
+    assert encrypt_message("teste", 1) == "t_etse"
