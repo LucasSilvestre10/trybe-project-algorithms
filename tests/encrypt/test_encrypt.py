e1b1ff950324a50098af79554d71a04ccc75ba1a
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-06-11 01:54:54 -0300

    correções de lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,7 +5,5 @@ from challenges.challenge_encrypt_message import encrypt_message
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(0, "string")
-
-   
-    assert encrypt_message("teste", -1) == "etset"   
+    assert encrypt_message("teste", -1) == "etset"
     assert encrypt_message("teste", 1) == "t_etse"
